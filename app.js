1779f97ed787f12f260d96924d57b3613c11a11f
rladbgus <cocokiuuu1858@gmail.com>
2020-08-12 19:58:25 +0900

    ass setting

--- app.js
+++ app.js
@@ -12,14 +12,15 @@ dotenv.config();
 console.log(process.env.SESSION_SECRET_KEY, '222222');
 
 app.use(cors());
-app.use(cookieParser('1234'));
+app.use(cookieParser(process.env.SESSION_SECRET_KEY));
 app.use(session({
-  secret: '1234',
+  secret: process.env.SESSION_SECRET_KEY,
   resave: false, // resave는 요청이 왔을 때 세션에 수정 사항이 생기지 않더라도 세션을 다시 저장할지에 대한 설정
   saveUninitialized: false, // saveUninitialized는 세션에 저장할 내역이 없더라도 세션을 저장할지에 대한 설정
 }));
 app.use(logger());
-// app.use(express.json());
+app.use(express.json()); // body-parser역할
+app.use(express.urlencoded({ extended: false }));
 
 app.post('/', (req, res) => {
   console.log(req.body, '22222');
