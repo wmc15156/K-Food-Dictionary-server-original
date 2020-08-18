9562fd934e6760a55517a1ae6ec85ad92dbabd86
Your Name <you@your-domain.com>
2020-08-18 22:30:30 +0900

    Feat: jwt인증 방식으로 로그인을 구현했습니다.
     - 서버와 클라이언트간 쿠키공유 문제로 인하여 인증방식을 세션 => 토큰방식으로 변경하였습니다.

--- controller/users/login.js
+++ controller/users/login.js
@@ -1,30 +1,54 @@
 const { User } = require('../../models');
 const dotenv = require('dotenv');
 const crypto = require('crypto');
-
+const jwt = require('jsonwebtoken');
 dotenv.config();
 
 module.exports = {
     post: async (req, res) => {
-        User.findOne({
-            where: {
-                email: req.body.email,
-                password: crypto.createHash('sha256').update(req.body.password).digest('hex')
-            }
-        })
-            .then(data => {
-                if (!data) {
-                    res.status(404).send('unvalid user')
-                } else {
-                    req.session.userid = data.email
-                    res.status(200).send({
-                        id: data.id,
-                        username: data.username
-                    })
-                }
-            })
-            .catch(error => {
-                res.status(500).send(error);
-            })
+      console.log('여기');
+      const { email , password } = req.body;
+
+      // jwt 발급
+      let token = jwt.sign({
+        // email : email = email => 키와벨류값이 같으면 email로 생략가능 ES6문법
+        email
+      }, 'qlalfdldi', {
+        expiresIn: '20m'
+      });
+
+      const oneUser = await User.findOne({
+        where: {email,
+          password: crypto.createHash('sha256').update(password).digest('hex')}
+      });
+      if(!oneUser) {
+        console.log('여기');
+       return res.status(404).send('unvaild user');
+      } else {
+        return res.status(200).json({ success: true, token })
+      }
+       
     }
 };
+
+// User.findOne({
+//   where: {
+//       email: req.body.email,
+//       password: crypto.createHash('sha256').update(req.body.password).digest('hex')
+//   }
+// })
+//   .then(data => {
+//       if (!data) {
+//           res.status(404).send('unvalid user')
+//       } else {
+//           req.session.userid = data.email
+//           res.cookie('user','김현진');
+//           res.status(200).send({
+//               id: data.id,
+//               username: data.username
+//           })
+//       }
+//   })
+//   .catch(error => {
+//       res.status(500).send(error);
+//   })
